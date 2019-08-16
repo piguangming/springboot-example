cc3c3887bfa0a0369657bcdccbaa35cc07e1094f
piguangming <piguangming@hotmail.com>
2019-08-16 21:47:40 +0800

    文件上传 Demo上线， 但是没跑通

--- upload-large-files/src/main/java/upload/UploadController.java
+++ upload-large-files/src/main/java/upload/UploadController.java
@@ -1,10 +1,7 @@
 package upload;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -22,10 +19,11 @@ public class UploadController {
     }
 
     @PostMapping("/upload")
+    @ResponseBody
     public String upload(@RequestParam("file") MultipartFile file) throws Exception {
         Path path = Paths.get("C://temp//" + file.getOriginalFilename());
         Files.write(path, file.getBytes());
-        return "上传成功";
+        return "upload success";
     }
 
 
